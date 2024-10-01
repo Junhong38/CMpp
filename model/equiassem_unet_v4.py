ec1d17519c10af5e04b420d5a232cdfcfca6950e
NahyukLEE <nahyuk.lee@postech.ac.kr>
2024-10-01 15:21:28 +0900

    misc

--- model/equiassem_unet_v4.py
+++ model/equiassem_unet_v4.py
@@ -295,7 +295,7 @@ class EquiAssem_unet_v4(pl.LightningModule):
         # 9. Weighted SVD with top-k correspondence selections
         with torch.no_grad():
             src_corr_pts, trg_corr_pts, corr_scores, estimated_transform, pred_corr = self.fine_matching(
-                src_pcd, trg_pcd, matching_scores_drop, k=128)
+                src_pcd, trg_pcd, matching_scores_drop, confidence,  k=128)
 
         out_dict['estimated_rotat'] = estimated_transform[:3, :3].T
         out_dict['estimated_trans'] = -(estimated_transform[:3, :3].inverse() @ -estimated_transform[:3, 3])
