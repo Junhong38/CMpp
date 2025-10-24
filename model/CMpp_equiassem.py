1abe99f016644dda3fe2c3c2878f6edc25c34268
kimsangki <kimsangki@postech.ac.kr>
2025-10-24 20:09:38 +0900

    REMOVE: Remove redundant code only for debugging

--- model/CMpp_equiassem.py
+++ model/CMpp_equiassem.py
@@ -461,7 +461,9 @@ class EquiAssem(pl.LightningModule):
     
 
     def on_train_batch_end(self, outputs, batch, batch_idx):
-        self.check_grad_and_nan()
+        pass
+        # If you want to check the gradient and NaN, uncomment the following line
+        # self.check_grad_and_nan()
 
     
     def check_grad_and_nan(self):
@@ -610,11 +612,6 @@ class EquiAssem(pl.LightningModule):
         src_equi_feats_backbone = self.backbone(src_pcd) # (1, C, 3, N)
         trg_equi_feats_backbone = self.backbone(trg_pcd) # (1, C, 3, M)
 
-
-        # Check for Inf or Nan
-        check_inf_or_nan(src_equi_feats_backbone, 'src_equi_feats_backbone')
-        check_inf_or_nan(trg_equi_feats_backbone, 'trg_equi_feats_backbone')
-
         
         if self.additional_VNLinearLeakyReLU: # 2. Frame Prediction
             # 2-1. Merge global information by averaging
@@ -632,11 +629,6 @@ class EquiAssem(pl.LightningModule):
         else: # 2. Basis Vector Projection 
             src_vecs = self.proj(src_equi_feats_backbone).permute(0, 3, 1, 2) # (1, N, 2, 3)
             trg_vecs = self.proj(trg_equi_feats_backbone).permute(0, 3, 1, 2) # (1, M, 2, 3)
-        
-
-        # Check for Inf or Nan
-        check_inf_or_nan(src_vecs, 'src_vecs')
-        check_inf_or_nan(trg_vecs, 'trg_vecs')
 
         
         # 3. Calculate equivariant shape features
@@ -644,19 +636,9 @@ class EquiAssem(pl.LightningModule):
         trg_equi_feats = self.equi_layer(trg_equi_feats_backbone.unsqueeze(-1)).squeeze(-1) # (1, C, 3, M)
 
 
-        # Check for Inf or Nan
-        check_inf_or_nan(src_equi_feats, 'src_equi_feats')
-        check_inf_or_nan(trg_equi_feats, 'trg_equi_feats')
-
-
         # 4. Gram Schmidt & Cross-product, this is for making three basis vectors by using two predicted vectors
         src_ori = ortho2rotation(src_vecs, optimum=self.use_opt_gram) # (1, N, 2, 3) -> (1, N, 3, 3)
         trg_ori = ortho2rotation(trg_vecs, optimum=self.use_opt_gram) # (1, M, 2, 3) -> (1, M, 3, 3)
-
-
-        # Check for Inf or Nan
-        check_inf_or_nan(src_ori, 'src_ori')
-        check_inf_or_nan(trg_ori, 'trg_ori')
 
 
         # Save for visualization
@@ -669,11 +651,6 @@ class EquiAssem(pl.LightningModule):
         trg_inv_feats = torch.matmul(trg_equi_feats.permute(0, 3, 1, 2).float(), trg_ori.transpose(-2,-1).float()) # (1, M, C, 3) x (1, M, 3, 3) -> (1, M, C, 3)
         src_inv_feats = rearrange(src_inv_feats, 'b n c r -> b (c r) n') # (1, N, C, 3) -> (1, C*3, N)
         trg_inv_feats = rearrange(trg_inv_feats, 'b n c r -> b (c r) n') # (1, M, C, 3) -> (1, C*3, M)
-
-
-        # Check for Inf or Nan
-        check_inf_or_nan(src_inv_feats, 'src_inv_feats')
-        check_inf_or_nan(trg_inv_feats, 'trg_inv_feats')
 
 
         # OPTIONAL 5. Chaneel Attention Map
@@ -691,11 +668,6 @@ class EquiAssem(pl.LightningModule):
         trg_shape_feats = self.shape_mlp(trg_inv_feats) # # (1, C*3, M) -> (1, D, N)
         if self.attention == 'channel': # (1, D, M) * channel attention
             trg_shape_feats = trg_shape_feats * shape_attention
-        
-
-        # Check for Inf or Nan
-        check_inf_or_nan(src_shape_feats, 'src_shape_feats')
-        check_inf_or_nan(trg_shape_feats, 'trg_shape_feats')
 
 
         if not self.delete_occupancy_loss:
@@ -707,11 +679,6 @@ class EquiAssem(pl.LightningModule):
             trg_occ_feats = self.occ_mlp(trg_inv_feats) # (1, 1023, M) -> (1, 512, M)
             if self.attention == 'channel': 
                 trg_occ_feats = trg_occ_feats * occ_attention
-            
-
-            # Check for Inf or Nan
-            check_inf_or_nan(src_occ_feats, 'src_occ_feats')
-            check_inf_or_nan(trg_occ_feats, 'trg_occ_feats')
         
 
         # 7. Optimal Transport
