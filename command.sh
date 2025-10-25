f020c30308e23daa7f49668b0fb8f06dde995824
kimsangki <kimsangki@postech.ac.kr>
2025-10-25 21:11:28 +0900

    TEST: FULL AND SMALL WITH NEW VERSION

--- command.sh
+++ command.sh
@@ -91,18 +91,14 @@ rm -rf checkpoint/T4_S_AVNOON_NCD_NPM_NODOCC_OG/ && CUDA_VISIBLE_DEVICES=2 pytho
 rm -rf checkpoint/T4D0_S_AVNOON_NCD_NPM_NODOCC_OG/ && CUDA_VISIBLE_DEVICES=3 python main.py --model CMpp_equiassem --logpath T4D0_S_AVNOON_NCD_NPM_NODOCC_OG --scale small --multiplicity 1 --epochs 0 --visualize --only_one_norm --detach_mode --use_opt_gram --wandb --wandb_project CMpp &
 end
 
+:<<end
+rm -rf checkpoint/DET_T5_S_CMoriginByCMpp/ && CUDA_VISIBLE_DEVICES=0 python main.py --model CMpp_equiassem --logpath DET_T5_S_CMoriginByCMpp --scale small --multiplicity 1 --epochs 0 --visualize --scheduler_mode CM --deterministic --wandb --wandb_project CMpp &
+rm -rf checkpoint/DET_T5_S_CMoriginByCMpp_MS/ && CUDA_VISIBLE_DEVICES=1 python main.py --model CMpp_equiassem --logpath DET_T5_S_CMoriginByCMpp_MS --scale small --multiplicity 1 --epochs 0 --visualize --scheduler_mode CM --deterministic --move_smaller --wandb --wandb_project CMpp &
 
-rm -rf checkpoint/DET_T4_S_CMoriginByCMpp/ && CUDA_VISIBLE_DEVICES=0 python main.py --model CMpp_equiassem --logpath DET_T4_S_CMoriginByCMpp --scale small --multiplicity 1 --epochs 0 --visualize --scheduler_mode CM --deterministic --wandb --wandb_project CMpp &
-rm -rf checkpoint/DET_T4_S_CMoriginByCMpp_MS/ && CUDA_VISIBLE_DEVICES=1 python main.py --model CMpp_equiassem --logpath DET_T4_S_CMoriginByCMpp_MS --scale small --multiplicity 1 --epochs 0 --visualize --scheduler_mode CM --deterministic --move_smaller --wandb --wandb_project CMpp &
-
-rm -rf checkpoint/DET_N_T4_S_CMoriginByCMpp/ && CUDA_VISIBLE_DEVICES=2 python main.py --model CMpp_equiassem --logpath DET_N_T4_S_CMoriginByCMpp --scale small --multiplicity 1 --epochs 0 --visualize --scheduler_mode CM --wandb --wandb_project CMpp &
-rm -rf checkpoint/DET_N_T4_S_CMoriginByCMpp_MS/ && CUDA_VISIBLE_DEVICES=3 python main.py --model CMpp_equiassem --logpath DET_N_T4_S_CMoriginByCMpp_MS --scale small --multiplicity 1 --epochs 0 --visualize --scheduler_mode CM --move_smaller --wandb --wandb_project CMpp &
-
-rm -rf checkpoint/DET_T4_S_AVNOON_NCD_NPM_NODOCC_OG/ && CUDA_VISIBLE_DEVICES=4 python main.py --model CMpp_equiassem --logpath DET_T4_S_AVNOON_NCD_NPM_NODOCC_OG --scale small --multiplicity 1 --epochs 0 --visualize --only_one_norm --detach_mode --use_opt_gram --deterministic --wandb --wandb_project CMpp &
-rm -rf checkpoint/DET_T4_S_AVNOON_NCD_NPM_NODOCC_OG_MS/ && CUDA_VISIBLE_DEVICES=5 python main.py --model CMpp_equiassem --logpath DET_T4_S_AVNOON_NCD_NPM_NODOCC_OG_MS --scale small --multiplicity 1 --epochs 0 --visualize --only_one_norm --detach_mode --use_opt_gram --deterministic --move_smaller --wandb --wandb_project CMpp &
+rm -rf checkpoint/DET_T5_S_AVNOON_NCD_NPM_NODOCC_OG/ && CUDA_VISIBLE_DEVICES=2 python main.py --model CMpp_equiassem --logpath DET_T5_S_AVNOON_NCD_NPM_NODOCC_OG --scale small --multiplicity 1 --epochs 0 --visualize --only_one_norm --detach_mode --use_opt_gram --deterministic --wandb --wandb_project CMpp &
+rm -rf checkpoint/DET_T5_S_AVNOON_NCD_NPM_NODOCC_OG_MS/ && CUDA_VISIBLE_DEVICES=3 python main.py --model CMpp_equiassem --logpath DET_T5_S_AVNOON_NCD_NPM_NODOCC_OG_MS --scale small --multiplicity 1 --epochs 0 --visualize --only_one_norm --detach_mode --use_opt_gram --deterministic --move_smaller --wandb --wandb_project CMpp &
+end
 
 
-
-rm -rf checkpoint/DEBUG/ && CUDA_VISIBLE_DEVICES=4 python main.py --model CMpp_equiassem --logpath DEBUG --scale small --multiplicity 1 --epochs 0 --visualize --only_one_norm --detach_mode --use_opt_gram --deterministic
-
+rm -rf checkpoint/DET_T5_S_AVNOON_NCD_NPM_NODOCC_OG/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --model CMpp_equiassem --logpath DET_T5_S_AVNOON_NCD_NPM_NODOCC_OG --scale full --multiplicity 1 --epochs 0 --visualize --only_one_norm --detach_mode --use_opt_gram --deterministic --gpus 0 1 2 3 4 5 6 7
 
