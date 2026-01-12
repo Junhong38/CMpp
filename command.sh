af8028e32b6c3c30d021e6e3e3cec236e3bf924a
kimsangki <kimsangki@postech.ac.kr>
2026-01-12 19:57:32 +0900

    TEST: 2nd stage

--- command.sh
+++ command.sh
@@ -1,6 +1,6 @@
 
 # 2 stage training
-
+:<<end
 # 1st stage -> orientation training
 rm -rf checkpoint/FIRST_G8NDB6SN_NPCFNR1-2C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --only_train_normal --logpath FIRST_G8NDB6SN_NPCFNR1-2C1 --epochs 0 --scheduler_mode none --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 6 --flip_normal_mode rightv1_2 --consistency_loss_weight 1.0 --wandb --wandb_entity CMppProject --wandb_project CMpp
 rm -rf checkpoint/FIRST_G8NDB6SN_NPCFNR1-3C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --only_train_normal --logpath FIRST_G8NDB6SN_NPCFNR1-3C1 --epochs 0 --scheduler_mode none --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 6 --flip_normal_mode rightv1_3 --consistency_loss_weight 1.0 --wandb --wandb_entity CMppProject --wandb_project CMpp
@@ -8,17 +8,17 @@ rm -rf checkpoint/FIRST_G8NDB6SN_NPCFNR2C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,
 rm -rf checkpoint/FIRST_G8NDB6SN_NPCFNR3C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --only_train_normal --logpath FIRST_G8NDB6SN_NPCFNR3C1 --epochs 0 --scheduler_mode none --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 6 --flip_normal_mode rightv3 --consistency_loss_weight 1.0 --wandb --wandb_entity CMppProject --wandb_project CMpp
 rm -rf checkpoint/FIRST_G8NDB6SN_NPCFNR4C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --only_train_normal --logpath FIRST_G8NDB6SN_NPCFNR4C1 --epochs 0 --scheduler_mode none --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 6 --flip_normal_mode rightv4 --consistency_loss_weight 1.0 --wandb --wandb_entity CMppProject --wandb_project CMpp
 rm -rf checkpoint/FIRST_G8NDB6SN_NPCFNR5C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --only_train_normal --logpath FIRST_G8NDB6SN_NPCFNR5C1 --epochs 0 --scheduler_mode none --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 6 --flip_normal_mode rightv5 --consistency_loss_weight 1.0 --wandb --wandb_entity CMppProject --wandb_project CMpp
-
-
-
-:<<end
-# 2nd stage -> shape training
-rm -rf checkpoint/SECOND_G8NDB3_NPCFNR1-2C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint/FIRST_G8NDB6SN_NPCFNR1-2C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR1-2C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv1_2 --wandb --wandb_entity CMppProject --wandb_project CMpp
-rm -rf checkpoint/SECOND_G8NDB3_NPCFNR1-3C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint/FIRST_G8NDB6SN_NPCFNR1-3C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR1-3C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv1_3 --wandb --wandb_entity CMppProject --wandb_project CMpp
-rm -rf checkpoint/SECOND_G8NDB3_NPCFNR2C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint/FIRST_G8NDB6SN_NPCFNR2C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR2C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv2 --wandb --wandb_entity CMppProject --wandb_project CMpp
-rm -rf checkpoint/SECOND_G8NDB3_NPCFNR3C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint/FIRST_G8NDB6SN_NPCFNR3C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR3C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv3 --wandb --wandb_entity CMppProject --wandb_project CMpp
-rm -rf checkpoint/SECOND_G8NDB3_NPCFNR4C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint/FIRST_G8NDB6SN_NPCFNR4C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR4C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv4 --wandb --wandb_entity CMppProject --wandb_project CMpp
-rm -rf checkpoint/SECOND_G8NDB3_NPCFNR5C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint/FIRST_G8NDB6SN_NPCFNR5C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR5C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv5 --wandb --wandb_entity CMppProject --wandb_project CMpp
 end
+
+
+
+# 2nd stage -> shape training
+# rm -rf checkpoint/SECOND_G8NDB3_NPCFNR1-2C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint_1stStage/FIRST_G8NDB6SN_NPCFNR1-2C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR1-2C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv1_2 --wandb --wandb_entity CMppProject --wandb_project CMpp
+# rm -rf checkpoint/SECOND_G8NDB3_NPCFNR1-3C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint_1stStage/FIRST_G8NDB6SN_NPCFNR1-3C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR1-3C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv1_3 --wandb --wandb_entity CMppProject --wandb_project CMpp
+# rm -rf checkpoint/SECOND_G8NDB3_NPCFNR2C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint_1stStage/FIRST_G8NDB6SN_NPCFNR2C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR2C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv2 --wandb --wandb_entity CMppProject --wandb_project CMpp
+# rm -rf checkpoint/SECOND_G8NDB3_NPCFNR3C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint_1stStage/FIRST_G8NDB6SN_NPCFNR3C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR3C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv3 --wandb --wandb_entity CMppProject --wandb_project CMpp
+rm -rf checkpoint/SECOND_G8NDB3_NPCFNR4C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint_1stStage/FIRST_G8NDB6SN_NPCFNR4C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR4C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv4 --wandb --wandb_entity CMppProject --wandb_project CMpp
+rm -rf checkpoint/SECOND_G8NDB3_NPCFNR5C1/ && CUDA_VISIBLE_DEVICES=0,1,2,3,4,5,6,7 python main.py --scale full --load_ori checkpoint_1stStage/FIRST_G8NDB6SN_NPCFNR5C1/models/last.ckpt --logpath SECOND_G8NDB3_NPCFNR5C1 --epochs 0 --scheduler_mode cos --gpus 0 1 2 3 4 5 6 7 --n_worker 6 --batch_size 3 --flip_normal_mode rightv5 --wandb --wandb_entity CMppProject --wandb_project CMpp
+
 
 
