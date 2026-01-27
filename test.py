6e30ad5388e3198c39364729bdd60804c15c7624
Junhong Lee <ellipsecycloid@gmail.com>
2026-01-28 02:33:11 +0900

    Revert "penetration ver2"
    This reverts commit bbf0c523f4f84a15e6e5b49262e150f4d568161a.

--- test.py
+++ test.py
@@ -99,8 +99,7 @@ def test(args):
                       using_seg_mode=args.using_seg_mode,
                       sampling_mode=args.sampling_mode,
                       normal_buffer=args.normal_buffer,
-                      penetration_buffer=args.penetration_buffer,
-                      use_penetration=args.use_penetration
+                      penetration_buffer=args.penetration_buffer
                       )
     
     # Wandb logger
@@ -220,7 +219,6 @@ if __name__ == '__main__':
     parser.add_argument('--using_seg_mode', type=str, default='threshold', choices=['threshold', 'weight', 'logit_and_mean', 'logit_and_sum'])
     parser.add_argument('--normal_buffer', type=int, default=0, help='Angle for normal thresholding buffer in penetration checking')
     parser.add_argument('--penetration_buffer', type=int, default=0, help='Angle for penetration depth buffer in penetration checking')
-    parser.add_argument('--use_penetration', action='store_true', help='If True ,use penetration rejecting for RANSAC')
 
 
     # Visualization arguments
